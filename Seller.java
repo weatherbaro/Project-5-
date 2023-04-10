f4ac21a873662e52cb4df6795430af0ad48b3d2e
ArtemKo23 <129921648+ArtemKo23@users.noreply.github.com>
2023-04-09 23:13:26 -0400

    Update Seller.java

--- Seller.java
+++ Seller.java
@@ -3,64 +3,21 @@ import java.util.ArrayList;
 
 public class Seller extends UserBase {
 
-    private String productName;
-    private String storeName;
-    private String description;
-    private int quantityAvail;
-    private int price;
-    private ArrayList<String> productList;
+    private String email
+    
+    private ArrayList<Store> stores;
 
-    public Seller (String email, String password, String nickname, String productName, String storeName, String description, int quantityAvail,
-                   int price){
+    public Seller (String email, String password, String nickname, ArrayList<Store> stores){
         super(email, nickname, password);
-        this.productName = productName;
-        productList = new ArrayList<>();
-        this.productList.add(productName);
-        this.storeName = storeName;
-        this.description = description;
-        this.quantityAvail = quantityAvail;
-        this.price = price;
+        this.stores = stores;
     }
-
-    public String getProductName(){
-        return productName;
+    
+    public ArrayList<Store> getStores() {
+        return stores;
     }
-    public String getStoreName(){
-        return storeName;
-    }
-    public String getDescription(){
-        return description;
-    }
-    public int getQuantityAvail(){
-        return quantityAvail;
-    }
-    public int getPrice(){
-        return price;
-    }
-    public void setProductName(String productName){
-        this.productName = productName;
-    }
-    public void setStoreName(String storeName) {
-        this.storeName = storeName;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setQuantityAvail(int quantityAvail) {
-        this.quantityAvail = quantityAvail;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-    public void deleteProduct(ArrayList<String> productList, String productName){
-        productList.remove(productName);
-        this.storeName = null;
-        this.description = null;
-        this.quantityAvail = 0;
-        this.price = 0;
+    
+    public void addStore(Store store) {
+        stores.add(store);
     }
 
     @Override
