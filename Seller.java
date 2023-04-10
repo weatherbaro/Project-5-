c2c0e848c31986528917a9dfe955d1a4e52ac295
weatherbaro <121148628+weatherbaro@users.noreply.github.com>
2023-04-10 13:25:44 -0400

    Update Seller.java

--- Seller.java
+++ Seller.java
@@ -1,23 +1,63 @@
+package com.company;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Seller extends UserBase {
 
-    private String email
-    
+    private String email;
     private ArrayList<Store> stores;
 
     public Seller (String email, String password, String nickname, ArrayList<Store> stores){
         super(email, nickname, password);
         this.stores = stores;
     }
-    
+
     public ArrayList<Store> getStores() {
         return stores;
     }
-    
+
     public void addStore(Store store) {
         stores.add(store);
+    }
+
+    public static void displayDashboard(ArrayList<Store> stores, String sortBy) {
+        try {
+            // displaying dashboard with sorting conditonals
+            if (sortBy.equals("normal")) {
+                System.out.println("Now displaying the dashboard...\n====================");
+            } else {
+                System.out.println("Sorting dashboard...\n====================");
+                // Sort Arraylist of Products with selection sort
+                int n = stores.size();
+                for (int i = 0; i < n-1; i++) {
+                    int min_idx = i;
+                    for (int j = i+1; j < n; j++) {
+                        if (stores.get(j).getsales() < stores.get(min_idx).getsales()) {
+                            min_idx = j;
+                        }
+                    }
+                    Collections.swap(stores, min_idx, i);
+                }
+                // reverses list to descending if needed
+                if (sortBy.equals("descending")) {
+                    Collections.reverse(stores);
+                }
+            }
+            for (Store x: stores) {
+                System.out.printf("Store: %s\nCustomers:\n", x.getName());
+
+                ArrayList<Product> storeProducts = x.getProducts();
+                System.out.printf("Products from %s:\n", x.getName());
+                for (Product p: storeProducts) {
+                    System.out.printf("%s - %d sold\n", p.getName(), p.getQuantitySold());
+                }
+
+            }
+            System.out.println("====================");
+        } catch (Exception e) {
+            System.out.println("There was an error displaying the dashboard!");
+        }
     }
 
     @Override
