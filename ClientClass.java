2e8bbb448d5d7acc4187c48994b0bd7c6d4cc95f
ArtemKo23 <129921648+ArtemKo23@users.noreply.github.com>
2023-04-21 14:11:21 -0400

    Create ClientClass.java

---
+++ ClientClass.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class ClientClass {
+  
+  public static void main(String[] args) {
+    
+    
+    
+  }
+  
+  private static final String[] loginMenu = {"Login", "Create An Account", "Exit"};
+    private static final String[] customerMenu = {"1. Sort products", "2. Search for a product", "3. Display Dashboard",
+            "4. Select a product", "5. Account Settings", "6. View purchase history", "Exit"};
+    private static final String[] productSort = {"1. Sale filter", "2. Sort by ascending price",
+            "3. Sort by descending price", "4. Sort by ascending quantity", "5. Sort by descending quantity",
+            "6. Show normal marketplace", "Return"};
+    private static final String[] dashboard = {"1. Sort by ascending sales", "2. Sort by descending sales",
+            "3. Return to dashboard menu"};
+    private static final String[] sellerMenu = {"1. Create a store", "2. Select a store", "3. Display Dashboard",
+            "4. Account Settings", "5. Logout"};
+    private static final String[] storeMenu = {"1. Create a product", "2. Edit a product", "3. Delete a product",
+            "4. Choose a different store", "5. Return to Seller Menu"};
+  
+  
+  
+  
+}
