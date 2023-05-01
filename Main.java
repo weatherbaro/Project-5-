56242a39fd5bb73a664d9dbda06d29b0d9a95fa8
weatherbaro <121148628+weatherbaro@users.noreply.github.com>
2023-05-01 14:51:54 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -802,6 +802,9 @@ public class Main implements Runnable{
                                             oos.writeBoolean(purchased);
                                             if (purchased) {
                                                 System.out.println("The product has been successfully purchased!\nReturning to the marketplace...");
+                                                updateCustomerHistory(email, purchaseHistory);
+                                                updateCustomerProducts(productDirectory);
+                                                updateCustomerStores(storeDirectory);
                                             } else {
                                                 System.out.println("The product could not be purchased!\n Returning to the marketplace...");
                                             }
@@ -917,12 +920,18 @@ public class Main implements Runnable{
                                     Store store = new Store(storeName, 0, new ArrayList<>(), new ArrayList<>());
                                     seller.addStore(store);
                                     System.out.println("Your new store has been created!");
+                                    updateSellerAccount(email, ownedStores);
+                                    updateSellerStores(storeDirectory, ownedStores);
+                                    updateSellerProducts(productDirectory, ownedProducts, deletedProducts);
                                 }
                                 case "2" -> {
                                     System.out.println("Enter the name of the store:");
                                     String selectedStore = (String)ois.readObject();
                                     boolean productMenu = true;
                                     while (productMenu) {
+                                        updateSellerAccount(email, ownedStores);
+                                        updateSellerStores(storeDirectory, ownedStores);
+                                        updateSellerProducts(productDirectory, ownedProducts, deletedProducts);
                                         System.out.println("""
                                             1. Create a product
                                             2. Edit a product
@@ -950,6 +959,9 @@ public class Main implements Runnable{
                                                         try {
                                                             s.addProduct(toBeAdded);
                                                             System.out.println("Product has been added.");
+                                                            updateSellerAccount(email, ownedStores);
+                                                            updateSellerStores(storeDirectory, ownedStores);
+                                                            updateSellerProducts(productDirectory, ownedProducts, deletedProducts);
                                                         } catch (NotAProductException e) {
                                                             System.out.println(e);
                                                         }
@@ -991,6 +1003,9 @@ public class Main implements Runnable{
                                                     }
                                                 }
                                                 System.out.println("The product has been edited.");
+                                                updateSellerAccount(email, ownedStores);
+                                                updateSellerStores(storeDirectory, ownedStores);
+                                                updateSellerProducts(productDirectory, ownedProducts, deletedProducts);
                                             }
                                             case "3" -> {
                                                 System.out.println("Enter the name of the product to be deleted:");
@@ -1006,6 +1021,9 @@ public class Main implements Runnable{
                                                     }
                                                 }
                                                 System.out.println("The product has been deleted.");
+                                                updateSellerAccount(email, ownedStores);
+                                                updateSellerStores(storeDirectory, ownedStores);
+                                                updateSellerProducts(productDirectory, ownedProducts, deletedProducts);
                                             }
                                             case "4" -> {
                                                 System.out.println("Enter the path of the file to be imported:");
@@ -1036,6 +1054,9 @@ public class Main implements Runnable{
                                                     br.close();
                                                     importFile.delete();
                                                     System.out.println("The product(s) in the file have been imported!");
+                                                    updateSellerAccount(email, ownedStores);
+                                                    updateSellerStores(storeDirectory, ownedStores);
+                                                    updateSellerProducts(productDirectory, ownedProducts, deletedProducts);
                                                 } catch (Exception e) {
                                                     System.out.println("Error importing new products!");
                                                 }
@@ -1067,6 +1088,9 @@ public class Main implements Runnable{
                                                     }
                                                     bw.close();
                                                     System.out.println("Products have been successfully exported!");
+                                                    updateSellerAccount(email, ownedStores);
+                                                    updateSellerStores(storeDirectory, ownedStores);
+                                                    updateSellerProducts(productDirectory, ownedProducts, deletedProducts);
                                                 } catch (Exception e) {
                                                     System.out.println("Error exporting products!");
                                                 }
